0e21a50ffbaba85fc6d60f3be28390026613bdf7
Sam <samgomezs7@hotmail.com>
2023-09-12 10:45:37 -0600

    mappers

--- lib/infrastructure/datasource/moviedb_datasource.dart
+++ lib/infrastructure/datasource/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movies.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MovieDatasource {
@@ -12,7 +14,11 @@ class MoviedbDatasource extends MovieDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
-    return [];
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-post')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
+    return movies;
   }
 }
---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movies.dart';
+import 'package:cinemapedia/domain/repositories/movie_repository.dart';
+
+class MovieRepositryImpl extends MovieRepository {
+  final MovieDatasource datasource;
+
+  MovieRepositryImpl(this.datasource);
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
